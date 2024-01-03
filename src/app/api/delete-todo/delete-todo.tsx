978a240696516f4ad134bc2b683287abe8334886
irfan7k7 <unknown7k7k@gmail.com>
2024-01-03 11:15:22 +0530

    todo method added

---
+++ src/app/api/delete-todo/delete-todo.tsx
@@ -0,0 +1,18 @@
+import TodoModel from "@/model/todo/todoModel";
+import { connectDb } from "@/utils/mongoose/dbConnect";
+import mongoose from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { isConnected } = await connectDb();
+    const todoDetails = await req.json();
+
+    const todoObjectId = new mongoose.Types.ObjectId(todoDetails._id);
+    await TodoModel.deleteOne({ _id: todoObjectId });
+    return NextResponse.json({ isDeleted: true });
+  } catch (error) {
+    console.log("error", error);
+    return NextResponse.json({ isValid: false }, { status: 400 });
+  }
+}
